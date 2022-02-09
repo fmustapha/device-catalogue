978fc86bd14994acc386f2e5e7fd16260d925add
Faith Yusuf <faithyusuf@xennialconsulting.com>
2022-02-08 23:15:53 -0500

    update routes add watches link

--- src/ui/components/App/index.js
+++ src/ui/components/App/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Home from '../Home';
+import Watches from '../Watches';
 import NotFound from '../NotFound';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/">
-        <Home />
-      </Route>
+      <Route path="/watches"><Watches /></Route>
+      <Route path="/notFound"><NotFound /></Route>
+      <Route path="/"><Home /></Route>
+      <Redirect to="notFound" />
     </Switch>
   </Router>
 );
