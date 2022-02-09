8c53a8f84b471513343d2fad8bc8241f71a65d29
Faith Yusuf <faithyusuf@xennialconsulting.com>
2022-02-08 23:16:44 -0500

    add watches link in home page

--- src/ui/components/Home/index.js
+++ src/ui/components/Home/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
 import Heading from '@tds/core-heading';
 
-const Home = () => <Heading level="h1">Device Catalogue</Heading>;
+const Home = () => {
+
+  return(
+    <Fragment>
+      <Heading level="h1">Device Catalogue</Heading>
+      <Link to="/watches"> Watches </Link>
+    </Fragment>
+  );
+};
+
 
 export default Home;
