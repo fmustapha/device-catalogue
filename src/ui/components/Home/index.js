9ed1adccc81c9f9a06684dc876b68a8dda564a16
Faith Yusuf <faithyusuf@xennialconsulting.com>
2022-02-10 17:28:08 -0500

    feature: add iphones route and link

--- src/ui/components/Home/index.js
+++ src/ui/components/Home/index.js
@@ -1,17 +1,19 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 
-import Heading from '@tds/core-heading';
+import Heading from "@tds/core-heading";
+import Box from "@tds/core-box";
 
 const Home = () => {
-
-  return(
+  return (
     <Fragment>
       <Heading level="h1">Device Catalogue</Heading>
-      <Link to="/watches"> Watches </Link>
+      <Box between={2}>
+        <Link to="/watches"> Watches </Link>
+        <Link to="/iphones"> iPhones</Link>
+      </Box>
     </Fragment>
   );
 };
-
 
 export default Home;
