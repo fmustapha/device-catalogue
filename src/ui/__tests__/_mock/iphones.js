d6050c6686fad0c49c4646b3c7c769484c657b9c
Faith Yusuf <faithyusuf@xennialconsulting.com>
2022-02-10 23:29:02 -0500

    chore(test): add api data mocks

---
+++ src/ui/__tests__/_mock/iphones.js
@@ -0,0 +1,19 @@
+export default {
+  data: [
+    {
+      name: "iPhone 12 Pro",
+      brand: "Apple",
+      price: 1399,
+    },
+    {
+      name: "iPhone 12",
+      brand: "Apple",
+      price: 979,
+    },
+    {
+      name: "iPhone SE",
+      brand: "Apple",
+      price: 599,
+    },
+  ],
+};
---
+++ src/ui/__tests__/_mock/watches.js
@@ -0,0 +1,19 @@
+export default {
+  data: [
+    {
+      name: "Apple Watch Series 6",
+      brand: "Apple",
+      price: 529,
+    },
+    {
+      name: "Apple Watch SE",
+      brand: "Apple",
+      price: 369,
+    },
+    {
+      name: "Apple Watch Series 3",
+      brand: "Apple",
+      price: 259,
+    },
+  ],
+};
