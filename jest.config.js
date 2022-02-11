a0cf5c19727b7ee0905a63a756b74032c748d51d
Faith Yusuf <faithyusuf@xennialconsulting.com>
2022-02-10 23:45:06 -0500

    test: add file mock and configuration for jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  rootDir: './src',
-  setupFiles: [
-    path.resolve('jest/setupEnzyme.js')
-  ]
-}
+  rootDir: "./src",
+  setupFiles: [path.resolve("jest/setupEnzyme.js")],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  moduleDirectories: ["node_modules", "src"],
+};
